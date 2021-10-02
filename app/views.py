d65908497954dd6900a748bdeb0ef984f0bfb0c8
Shu-Ting Huang <sthuang890702@gmail.com>
2021-10-01 23:45:12 -0400

    Create matrix_input.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'matrix_size_input.html')
+    if request.method == 'GET':
+        return render(request, 'matrix_size_input.html')
+    elif request.method == 'POST':
+        if request.POST['form_submitted'] == 'matrix_size':
+            global m, n
+            m = int(request.POST['m'])
+            n = int(request.POST['n'])
+            return render(request, 'matrix_input.html', context={'range_m':range(m),'range_n':range(n)})
